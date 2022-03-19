56c213e89ee91b36984399b88adcbe8bea7ca882
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2022-03-19 06:12:38 +0530

    add sign in page

--- src/screens/public/loginrequired/index.js
+++ src/screens/public/loginrequired/index.js
@@ -1,9 +1,12 @@
 import { useContext, useEffect } from "react";
+import { OutLinedButton } from "../../../componenets/atoms/button/outLinedButton";
 import { AppBarContext } from "../../../context/appBarConfigProvider";
+import { ServiceLocator } from "../../../context/serviceProvider";
 import { LoginRequiredContainer, MessageBox } from "./styles";
 
 const LoginRequiredPage = () => {
   const { setAppBarConfigs } = useContext(AppBarContext);
+  const { authService } = useContext(ServiceLocator);
 
   useEffect(() => {
     setAppBarConfigs((prev) => {
@@ -16,6 +19,17 @@ const LoginRequiredPage = () => {
       <MessageBox>
         <h1>Login is required.</h1>
         <h2>Please Log in and try again</h2>
+
+        <OutLinedButton
+          name="Login"
+          backgroundColor="orange"
+          color="white"
+          clickCb={() => {
+            authService.federatedSignIn();
+          }}
+        >
+          sign in
+        </OutLinedButton>
       </MessageBox>
     </LoginRequiredContainer>
   );
